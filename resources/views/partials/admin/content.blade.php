cbcfbb8d408914548bc0c912f5d4364d7426e1da
Nox <juan.angarita.11@gmail.com>
2018-08-06 05:42:45 -0500

    No more suggestions

--- resources/views/partials/admin/content.blade.php
+++ resources/views/partials/admin/content.blade.php
@@ -6,11 +6,11 @@
             @yield('title')
             @yield('new_button')
             @if (!empty($suggestion_modules))
-                @foreach($suggestion_modules as $s_module)
+                <!--@foreach($suggestion_modules as $s_module)
                     <span class="new-button">
                         <a href="{{ url($s_module->action_url) . '?' . http_build_query((array) $s_module->action_parameters) }}" class="btn btn-default btn-sm" target="{{ $s_module->action_target }}"><span class="fa fa-rocket"></span> &nbsp;{{ $s_module->name }}</a>
                     </span>
-                @endforeach
+                @endforeach-->
             @endif
         </h1>
     </section>
